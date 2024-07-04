e126348bbe8476315b2ce0d83707827cb55966a2
d1ng0ls <4b4c4t3666@gmail.com>
2024-07-03 21:22:19 -0300

    pagina de perguntas frequentes

---
+++ assets/script/pages/faq/script.js
@@ -0,0 +1,21 @@
+function abre(id) {
+    const resp = document.getElementById("text" + id);
+    const quest = document.getElementById("quest" + id);
+
+    const todasAsRespostas = document.querySelectorAll('.text-quest');
+    todasAsRespostas.forEach(item => {
+        if (item.id !== 'text' + id) {
+            item.classList.remove('aberto');
+        }
+    });
+
+    const todasAsQuestoes = document.querySelectorAll('.faq-question');
+    todasAsQuestoes.forEach(item => {
+        if (item.id !== 'quest' + id) {
+            item.classList.remove('gira');
+        }
+    });
+
+    resp.classList.toggle("aberto");
+    quest.classList.toggle("gira");
+}
