0b6f14cee39895f1c4743947bf52847c84b31a6f
d1ng0ls <4b4c4t3666@gmail.com>
2024-07-21 21:27:02 -0300

    continuação index e atualização do sobre

--- assets/script/pages/index/script.js
+++ assets/script/pages/index/script.js
@@ -1,3 +1,31 @@
+// muda bg
+document.addEventListener('DOMContentLoaded', function() {
+    var home = document.getElementById('home');
+    if (home) {
+        var bgnum = Math.floor(Math.random() * 4);
+        console.log(bgnum);
+
+        switch (bgnum) {
+            case 0:
+                home.style.background = "url('../../assets/media/pages/index/banner_1.png') no-repeat center";
+                break;
+            case 1:
+                home.style.background = "url('../../assets/media/pages/index/banner_2.png') no-repeat center";
+                break;
+            case 2:
+                home.style.background = "url('../../assets/media/pages/index/banner_3.png') no-repeat center";
+                break;
+            case 3:
+                home.style.background = "url('../../assets/media/pages/index/banner_4.png') no-repeat center";
+                break;   
+        }
+        home.style.backgroundSize = "cover";
+    } else {
+        console.error("Elemento com ID 'home' não foi encontrado.");
+    }
+});
+
+// seleciona eventos
 function seleciona(id) {
     const resp = document.getElementById("eve" + id);
     const img = document.getElementById("img-event")
