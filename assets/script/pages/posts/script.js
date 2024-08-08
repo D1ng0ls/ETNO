74a3ee67e75fe5b0cffb2d271192a7ce3cd33875
Pedro Bonini <wakandadoceu@gmail.com>
2024-08-07 22:23:40 -0300

    papapapapapap

--- assets/script/pages/posts/script.js
+++ assets/script/pages/posts/script.js
@@ -1,57 +1,17 @@
-var itens = 5;
-var conta = 0;
-var lojas = document.querySelectorAll(".blocos");
+const button = document.querySelector('button');
+const popup = ducument.querySelector('.popup-wrapper');
 
-for(var x = 0; x < itens; x++) {
-    lojas[x].style.display = "block";
-}
+button.addEventListener('click', () => {
+    popup.style.display = 'block'
+}); 
 
-document.querySelector("#right-arrow").addEventListener("click", function() {
+popup.addEventListener('click', event => {
+    const classNameOfClickedElement = event.target.classList[0];
+    const classNames = ['popup-close', 'popup-wrapper', 'popup-link'];
+    const shouldClosePopup = classNames.some(classname =>
+        classname === classNameOfClickedElement);
 
-    var visiveis = document.querySelectorAll(".blocos");
-    if(conta < visiveis.length){
-        if(visiveis[conta].style.display == "block") {
-            conta++;
-        }
+    if (shouldClosePopup) {
+        popup.style.display = 'none';
     }
-
-    var limite = conta + itens > lojas.length - 1 ? lojas.length : conta + itens;
-
-    for(var x = conta; x < limite; x++) {
-        lojas[x].style.display = "block";
-    }
-
-    for(var x = 0; x < limite-itens; x++) {
-        lojas[x].style.display = "none";
-    }
-   
-    if(lojas.length - conta < itens) {
-        document.querySelector("#right-arrow").outerHTML = '';
-    }
-});
-
-document.querySelector("#left-arrow").addEventListener("click", function() {
-
-    var visiveis = document.querySelectorAll(".blocos");
-    if(conta < visiveis.length && conta > 0){
-        if(visiveis[conta].style.display == "block") {
-            conta--;
-        }
-    }
-
-    var limite = conta + itens > lojas.length - 1 ? lojas.length : conta + itens;
-
-    console.log(limite)
-    
-    for(var x = limite-1; x < limite; x--) {
-        lojas[x].style.display = "block";
-    }
-
-    for(var x = limite; x < itens; x--) {
-        lojas[x].style.display = "none";
-    }
-   
-    if(lojas.length - conta < itens) {
-        document.querySelector("#right-arrow").outerHTML = '';
-    }
-});
+})
