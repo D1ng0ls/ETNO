41565e6c34ef871a784ec8a1c2768315db39e6fe
d1ng0ls <4b4c4t3666@gmail.com>
2024-08-16 20:51:12 -0300

    atualizacao posts

--- assets/script/pages/posts/script.js
+++ assets/script/pages/posts/script.js
@@ -1,9 +1,12 @@
 function abreCard(idTipo, idPosi) {
-  const card = document.getElementById("card" + idTipo + idPosi);
+  const id = "card" + idTipo + idPosi;
+  const card = document.getElementById(id);
   const corpo = document.getElementById("corpo");
+  const btnClose = document.querySelector(`#${id} .fecha-card`)
 
   card.classList.toggle("cardAberto");
   corpo.classList.toggle("hidden");
+  btnClose.classList.toggle("aparece");
 
-  console.log(card)
+  console.log(btnClose)
 }
