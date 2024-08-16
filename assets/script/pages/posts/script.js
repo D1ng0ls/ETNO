0b594f4b804a5a7d8937daedf3c6cd5fc59d7daa
Pedro Bonini <wakandadoceu@gmail.com>
2024-08-16 20:03:06 -0300

    update

--- assets/script/pages/posts/script.js
+++ assets/script/pages/posts/script.js
@@ -1,9 +1,6 @@
 function abreCard(idTipo, idPosi) {
   const card = document.getElementById("card" + idTipo + idPosi);
-  const corpo = document.getElementById("corpo");
 
   card.classList.toggle("cardAberto");
   corpo.classList.toggle("hidden");
-
-  console.log(card)
 }
