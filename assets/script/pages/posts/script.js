f17da0b0ee627e04fdb53233e7e741b1b82ad792
d1ng0ls <4b4c4t3666@gmail.com>
2024-08-16 20:53:12 -0300

    aaaa

--- assets/script/pages/posts/script.js
+++ assets/script/pages/posts/script.js
@@ -1,5 +1,4 @@
 function abreCard(idTipo, idPosi) {
-<<<<<<< HEAD
   const id = "card" + idTipo + idPosi;
   const card = document.getElementById(id);
   const corpo = document.getElementById("corpo");
@@ -11,10 +10,3 @@ function abreCard(idTipo, idPosi) {
 
   console.log(btnClose)
 }
-=======
-  const card = document.getElementById("card" + idTipo + idPosi);
-
-  card.classList.toggle("cardAberto");
-  corpo.classList.toggle("hidden");
-}
->>>>>>> 0b594f4b804a5a7d8937daedf3c6cd5fc59d7daa
