a028b9cf1ab60975441d7177e83683ace8770df7
d1ng0ls <4b4c4t3666@gmail.com>
2024-08-17 18:55:09 -0300

    muitas coizinhas

--- assets/script/global/header.js
+++ assets/script/global/header.js
@@ -11,20 +11,31 @@ function myFunction() {
     }
 
     if (header.clientHeight > 0) {
-        document.getElementById('img-header').src = "assets/media/global/list.svg";
+        document.querySelector('.menu').classList.remove('menu-open');
+        document.querySelector('.menu').innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+      </svg>`;
     }
 }
 
 // Hamburger menu
 function burgermenu() {
     const header = document.getElementById('header');
-    const imgHeader = document.getElementById('img-header');
+    const imgHeader = document.querySelector('.menu');
 
     header.classList.toggle("abre-menu");
 
-    if (imgHeader.getAttribute('src') === "assets/media/global/list.svg") {
-        imgHeader.src = "assets/media/global/x.svg";
+    if (imgHeader.classList.contains('menu-open')) {
+        imgHeader.classList.remove('menu-open');
+        imgHeader.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+            </svg>`;
     } else {
-        imgHeader.src = "assets/media/global/list.svg";
+        imgHeader.classList.add('menu-open');
+        imgHeader.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+            </svg>`;
     }
 }
